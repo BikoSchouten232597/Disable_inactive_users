b25798f2c538d50ebe67b3886910b8ad67865562
BikoSchouten232597 <34523851+BikoSchouten232597@users.noreply.github.com>
2024-10-30 13:14:32 +0100

    Add dutch translation

---
+++ local_suspend_inactive_users_1730199255/lang/nl/local_suspend_inactive_users.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_suspend_inactive_users
+ * @category    string
+ * @copyright   2024 Biko Schouten
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Taalstrings voor de LDAP Connection plugin
+$string['pluginname'] = 'Schors inactieve gebruikers';
+$string['cron_task_success'] = 'Het schorsen van de gebruikers was succesvol';
+$string['cron_task_error'] = 'Er is een fout opgetreden bij het schorsen van de gebruikers';
+$string['suspend_inactive_users'] = 'Schors inactieve gebruikers';
